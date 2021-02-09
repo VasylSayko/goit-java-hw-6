47ecd728efaf7632bbdf9c21f27f41e768dc7c0f
VasylSayko <75450925+VasylSayko@users.noreply.github.com>
2021-02-09 14:09:30 +0200

    Update NameParserTest.java

--- NameParserTest.java
+++ NameParserTest.java
@@ -14,3 +14,12 @@ class NameParser {
         return sb.toString();
     }
 }
+
+class NameParserTest {
+    public static void main(String[] args) {
+        String[] names = {"John Doe", "Bill Mask", "Nigua Joshua"};
+
+        //John, Bill, Nigua
+        System.out.println(new NameParser().parse(names));
+    }
+}
